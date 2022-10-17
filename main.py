2a71a97c6eb830ffaa30a0df61653a0453aa5eab
Marco <fernstaedtmarco@gmail.com>
2022-10-17 09:00:20 -0700

    compleated UI

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from flask import g
+
+from pyparsing import col
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -20,12 +23,22 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", foreground=GREEN, fg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="White", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
 
+reset_button = Button(text="Restart", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
